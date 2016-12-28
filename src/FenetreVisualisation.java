c174bb7bcbc6193e8044adbe1b70707ee98a829f
Tamanoirkiller <ludovicburg0@gmail.com>
2016-12-28 19:13:44 +0100

    Ajout du boutton d'inscription

--- src/FenetreVisualisation.java
+++ src/FenetreVisualisation.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,13 +24,26 @@ public class FenetreVisualisation extends JFrame{
 
         this.setSize(700, 400);
         JPanel panGeneral = new JPanel();
-        panGeneral.setLayout(new GridLayout(2,2));
-        panGeneral.add(localisationArbrePanel.getArbrePanel());
+        panGeneral.setLayout(new GridLayout(2, 2));
+        JButton inscription = new JButton("Inscrir capteur(s)");
+        JPanel arbrePan = new JPanel();
+        arbrePan.add(localisationArbrePanel.getArbrePanel());
+        arbrePan.add(inscription);
+        panGeneral.add(arbrePan);
+
         panGeneral.add(tableauDonnees.getPanGlobal());
         panGeneral.add(alerte.getPanGlobal());
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.add(panGeneral);
+
+        inscription.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+            }
+        });
     }
 
     public void setProtocolManager(ProtocolManager protocolManager) {
